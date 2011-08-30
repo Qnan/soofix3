0d582111e9bbe7fc2db6556475f4a51cfd59f03e
Mikhail Kozhevnikov <amblerr@gmail.com>
2011-08-30 12:35:34 +0200

    cleanup, format, add find() method

--- src/soofix3/Tree.java
+++ src/soofix3/Tree.java
@@ -1,5 +1,6 @@
 package soofix3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,13 +12,36 @@ public final class Tree {
 	int pos;
 	Node root, ground;
 	static int INFINITY = Integer.MAX_VALUE;
+	public static boolean logBuilding = false;
 
 	public Tree(int[] seq) {
 		this.seq = seq;
 		build();
 	}
 
-	public Node edgeSplit(Suffix suffix, int token) {
+	public boolean contains(int[] seq) {
+		return find(seq) >= 0;
+	}
+
+	public int find(int[] seq) {
+		Node node = root;
+		int j = -1;
+		for (int i = 0; i < seq.length;) {
+			int token = seq[i];
+			if (!node.hasChild(token)) {
+				return -1;
+			}			
+			node = node.getChild(token);
+			for (j = node.startPos(); j < node.endPos() && i < seq.length; ++j, ++i) {
+				if (seq[i] != this.seq[j]) {
+					return -1;
+				}
+			}
+		}
+		return j - seq.length;
+	}
+	
+	private Node edgeSplit(Suffix suffix, int token) {
 		// i -> pos
 		// p -> pos - 1
 		// k -> suffix.from
@@ -25,12 +49,12 @@ public final class Tree {
 		// s' -> next
 		// r -> newNode
 		// return explicit state if not end point, otherwise null
-//		int firstToken = seq[suffix.from];
 		if (suffix.from >= pos - 1) {
-			if (suffix.node.hasChild(token))
+			if (suffix.node.hasChild(token)) {
 				return null; // end point
-			else
+			} else {
 				return suffix.node; // state already explicit
+			}
 		} else {
 			Node next = suffix.node.getChild(seq[suffix.from]);
 			int nextPos = next.startPos() + pos - suffix.from - 1;
@@ -43,21 +67,23 @@ public final class Tree {
 				next.setStartPos(nextPos);
 				newNode.setChild(seq[nextPos], next);
 				return newNode;
-			}			
+			}
 		}
 	}
-	
-	public void canonize (Suffix suffix) {
+
+	private void canonize(Suffix suffix) {
 		// s -> suffix.node
 		// k -> suffix.from
 		// p -> pos
 		while (true) {
 			int suffixLen = pos - suffix.from;
-			if (suffixLen == 0)
+			if (suffixLen == 0) {
 				return;
+			}
 			int firstToken = seq[suffix.from];
-			if (!suffix.node.hasChild(firstToken))
+			if (!suffix.node.hasChild(firstToken)) {
 				return;
+			}
 			Node nextNode = suffix.node.getChild(firstToken);
 			int nextNodeWordLen = nextNode.length(pos);
 			if (suffixLen < nextNodeWordLen) {
@@ -68,7 +94,7 @@ public final class Tree {
 		}
 	}
 
-	public void update(Suffix suffix) {
+	private void update(Suffix suffix) {
 		// s -> suffix.node
 		// k -> suffix.from
 		// i -> pos
@@ -78,20 +104,26 @@ public final class Tree {
 		Node explicitState = edgeSplit(suffix, token);
 		while (explicitState != null) {
 			explicitState.setChild(token, new Node(explicitState, pos - 1, INFINITY));
-			printTree();
-			if (oldRoot != root)
+			if (logBuilding) {
+				printTree();
+			}
+			if (oldRoot != root) {
 				nn.put(oldRoot, explicitState);
+			}
 			oldRoot = explicitState;
-			suffix.node = nn.get(suffix.node);// suffix.from);
+			suffix.node = nn.get(suffix.node);
 			canonize(suffix);
 			explicitState = edgeSplit(suffix, token);
-			System.out.println(".");
+			if (logBuilding) {
+				System.out.println(".");
+			}
 		}
-		if (oldRoot != root)
+		if (oldRoot != root) {
 			nn.put(oldRoot, suffix.node);
+		}
 	}
-	
-	public void build () {
+
+	private void build() {
 		ground = new Node(null, -1, -1);
 		root = new Node(ground, -1, 0);
 		for (int i = 0; i < seq.length; ++i) {
@@ -99,14 +131,18 @@ public final class Tree {
 		}
 		nn.put(root, ground);
 		Node current = root;
-		
+
 		Suffix suffix = new Suffix(current, 0);
-		for (pos = 1; pos < seq.length; ++pos) {			
-			System.out.println(nodeToString(suffix.node) + " -- " + suffix.from);
-			System.out.flush();
+		for (pos = 1; pos < seq.length; ++pos) {
+			if (logBuilding) {
+				System.out.println(nodeToString(suffix.node) + " -- " + suffix.from);
+				System.out.flush();
+			}
 			update(suffix);
 			canonize(suffix);
-			printTree();
+			if (logBuilding) {
+				printTree();
+			}
 		}
 		pos = seq.length; // TODO: unhack
 	}
@@ -127,56 +163,9 @@ public final class Tree {
 		}
 	}
 
-	public void printTree() {
+	private void printTree() {
 		printSubtree(root, 0);
 	}
-
-//	public void build() {
-//		root = new Node(null, 0, 0);
-//		nn.put(root, root);
-//		start = new Node(root, 0, seq.length);
-//		nn.put(start, root);
-//		root.setChild(seq[0], start);
-//		boolean stopFlag = false;
-//		Node activePoint = start;
-//		for (pos = 1; pos < seq.length; ++pos) {
-//			int token = seq[pos];
-//			Node current = activePoint, last = null;
-//			Node activePointNew = null;
-//			do {
-////				Node next = nn.get(current);
-////				stopFlag = current == root;
-////				if (!current.isLeaf()) {
-////					if (current.hasChild(token)) {
-////						Node child = current.getChild(token);
-////						if (child.endPos() - child.startPos() > 1) {
-////							Node newNode = edgeSplit(token, child, next);
-////							current = newNode;
-////						} else {
-////							nn.put(child, next);
-////							current = child;
-////						}
-////					} else {
-////						// create a new leaf node
-////						Node newNode = new Node(current, i, seq.length);
-////						nn.put(newNode, next);
-////						current.setChild(token, newNode);
-////						current = newNode;
-////					}
-////					if (activePointNew == null) {
-////						activePointNew = current;
-////					}
-////				}
-////
-////				if (last != null) {
-////					nn.put(last, current);
-////				}
-////				last = current;
-////				current = next;
-//			} while (!stopFlag);
-//			activePoint = activePointNew;
-//		}
-//	}
 
 	private void recPrint(StringBuilder sb, Node node) {
 		if (node.parent() != ground) {
@@ -188,31 +177,10 @@ public final class Tree {
 			sb.append("@");
 		}
 	}
-//
-//	private void recPrintAll(StringBuilder sb, Node node, int len) {
-//		for (int token : node.tokens()) {
-//			recPrintAll(sb, node, len);
-//		}
-//		if (node.parent() != null) {
-//			recPrint(sb, node.parent(), len);
-//			for (int i = node.startPos(); i < node.endPos(); ++i) {
-//				sb.append((char) seq[i]);
-//			}
-//		}
-//	}
 
 	private String nodeToString(Node node) {
 		StringBuilder sb = new StringBuilder();
 		recPrint(sb, node);
 		return sb.toString();
 	}
-
-//	public void printSuffixes() {
-//		Node current = start, last = null;
-//		while (last != root) {
-//			System.out.println("\"" + nodeToString(current) + "\"");
-//			last = current;
-//			current = nn.get(current);
-//		}
-//	}
 }
