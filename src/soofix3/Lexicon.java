068b6674c4bbcf3da1705bf4198be93c540d2901
Mikhail Kohzevnikov <amblerr@gmail.com>
2011-09-04 22:52:02 +0200

    lexicon class added

---
+++ src/soofix3/Lexicon.java
@@ -0,0 +1,33 @@
+package soofix3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lexicon {
+
+	final Map<String, Integer> lexicon = new HashMap<String, Integer>();
+	final Map<Integer, String> inv = new HashMap<Integer,String>();
+	final int size;
+
+	Lexicon(Iterable<String> tokens) {
+		lexicon.put(null, -1);
+		int cnt = 0;
+		for (String token : tokens) {
+			if (!lexicon.containsKey(token)) {
+				lexicon.put(token, cnt++);
+			}
+		}
+		for (String token : lexicon.keySet()) {
+			inv.put(lexicon.get(token), token);
+		}
+		size = cnt;
+	}
+
+	int id(String token) {
+		return lexicon.get(token);
+	}
+	
+	String token(int id) {
+		return inv.get(id);
+	}
+}
