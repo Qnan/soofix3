87939627cbe2844f6dd193104aba0155ca36ee69
Mikhail Kohzevnikov <amblerr@gmail.com>
2011-09-08 18:52:17 +0200

    jython interface for StringTree

---
+++ index.py
@@ -0,0 +1,20 @@
+__author__ = 'mikhail'
+import sys
+
+sys.path.append('dist/soofix3.jar')
+
+class index:
+    def __init__(self, documents):
+        from soofix3 import StringTree
+        self.st = StringTree(documents)
+
+    def add(self, document):
+        pass
+
+    def find(self, query):
+        print self.st.find(query);
+
+
+idx = index([['o','a','b','c'],['a','b'],['o','a','b','c']])
+idx.find(['a','b'])
+print 'done'
