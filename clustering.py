c67418b517f2dac9125a2c5b5c04f12d9a2cce25
Mikhail Kohzevnikov <amblerr@gmail.com>
2011-10-14 22:19:02 +0200

    use words rather then lemmata

--- clustering.py
+++ clustering.py
@@ -20,8 +20,8 @@ doc = None
 #ids = []
 cnt = 0
 id = -1
-outf = open("lines.txt",'wt')
-idf = open("ids.txt",'wt')
+outf = open("wines.txt",'wt')
+idf = open("wids.txt",'wt')
 for line in f:
     if num > 0 and cnt >= num:
         break
@@ -34,7 +34,7 @@ for line in f:
         id = line[2:].strip()
     else:
         if len(line.strip()) > 0:
-            word = line.split()[2]
+            word = line.split()[1].lower()
             outf.write(word + ' ')
 f.close()
 outf.close()
